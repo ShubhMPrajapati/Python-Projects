c18f451ab103ed20011cdb40d9691a62eb14cc5b
Shubham Prajapati <shubhamprajapati051@gmail.com>
2024-11-05 16:42:02 -0500

    Some changes made

--- Templating with jinja/main.py
+++ Templating with jinja/main.py
@@ -1,47 +1,18 @@
-from bs4 import BeautifulSoup
+from flask import Flask, render_template
 import requests
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 
-# Scraping Billboard 100
-date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0"}
-billboard_url = "https://www.billboard.com/charts/hot-100/" + date
-response = requests.get(url=billboard_url, headers=header)
+app = Flask(__name__)
 
-soup = BeautifulSoup(response.text, 'html.parser')
-song_names_spans = soup.select("li ul li h3")
-song_names = [song.getText().strip() for song in song_names_spans]
+@app.route('/')
+def home():
+    posts = requests.get('https://api.npoint.io/111dd683d74ef9d3a630').json()
+    return render_template("index.html",posts=posts)
 
-# Spotify Authentication
-sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        scope="playlist-modify-private",
-        redirect_uri="http://example.com",
-        client_id="753c5aa20b724f4db59529bf3b43ff4d",
-        client_secret="f7b7917fa7a14428897ea7a6291d1c80",
-        show_dialog=True,
-        cache_path="token.txt"
-    )
-)
-user_id = sp.current_user()["id"]
-print(user_id)
+@app.route('/post/<num>')
+def get_post(num):
+    number = int(num)
+    posts = requests.get('https://api.npoint.io/111dd683d74ef9d3a630').json()
+    return render_template("post.html", posts=posts, num=number)
 
-# Searching Spotify for songs by title
-song_uris = []
-year = date.split("-")[0]
-for song in song_names:
-    result = sp.search(q=f"track:{song} year:{year}", type="track")
-    print(result)
-    try:
-        uri = result["tracks"]["items"][0]["uri"]
-        song_uris.append(uri)
-    except IndexError:
-        print(f"{song} doesn't exist in Spotify. Skipped.")
-
-# Creating a new private playlist in Spotify
-playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
-print(playlist)
-
-# Adding songs found into the new playlist
-sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
+if __name__ == "__main__":
+    app.run(debug=False)
